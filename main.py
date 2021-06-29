a27e2debf4e85f7f800e9af90518b7e190c029fd
Lucas Arneiro <lucasarneiro.v@gmail.com>
2021-06-29 20:46:26 -0300

    Correcao calculo media

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 
 import numpy as np
 import pandas as pd
+from statistics import mean
 import sys
 import re
 from datetime import datetime
@@ -43,24 +44,37 @@ if __name__ == '__main__':
 
     # Lendo dataset e calculando valores
     dataset = pd.read_excel('Valores.xlsx')
-    minimo = dataset['VALORES'].min()
-    maximo = dataset['VALORES'].max()
-    #media = dataset['VALORES'].mean()
+
+    # Transformando a coluna 'VALORES' em lista, numerica, para calculo
+    a = dataset['VALORES'].str.replace(',', '.')
+    Valores_list = a.tolist()
+    cotacao = list(np.float_(a))
+
+    minimo = min(cotacao)
+    maximo = max(cotacao)
+    media = mean(cotacao)
 
     # Convertendo variaveis em string
     str_data_hora = repr(data_hora)
     str_min = repr(minimo)
     str_max = repr(maximo)
+    str_media = repr(media)
+ 
     # Escrevendo no arquivo log.txt
     arquivo = open("log.txt", "w")
-    arquivo.write("Inicio da Execucao: "+ str_data_hora + "\n")
-    arquivo.write("Valor minimo: " + str_min + "\nValor maximo: "+ str_max + "\n")
-    arquivo.write("Fim da Execucao: "+ str_data_hora + "\n")
+    arquivo.write("\nInicio da Execucao: "+ str_data_hora + "\n")
+    arquivo.write("Valor minimo: " + str_min + "\nValor maximo: "+ str_max + "\nValor Medio: "+ str_media)
+
+    # Marcando Tempo Final
+    data_hora_fim = datetime.now().strftime("%d/%m/%Y %H:%M")
+    str_data_hora_fim = repr(data_hora_fim)
+    arquivo.write("\nFim da Execucao: "+ str_data_hora_fim + "\n")
+    str_data_hora_fim = repr(data_hora_fim)
     arquivo.close()
 
 
     # Fim da Execução
-    print('\nFim da Execução: ', data_hora)
+    print('\nFim da Execução: ', data_hora_fim)
 
 
 
